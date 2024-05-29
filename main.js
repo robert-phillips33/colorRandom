61e9960ff08a0b6b04668f44374e1eb3c8b6106d
Carissa Hluchan <carissahluchan@gmail.com>
2024-05-28 18:29:56 -0600

    add: ruff draft of HTML and CSS

---
+++ main.js
@@ -0,0 +1,39 @@
+// document.queery selector's 
+
+
+
+
+
+
+
+
+
+
+
+// event listiner's
+
+
+
+
+
+
+
+
+
+
+
+// function's
+// Turing Hint:
+// Hex codes are 6 characters long, and each character is some value of 0-9 or A-F (ABCDEF0123456789). Don’t look up how to generate a random hex code - challenge yourselves to use pseudocode to problem-solve through it. This is where the learning happens!
+
+
+
+
+
+
+
+
+
+
+
+
